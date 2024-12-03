d7af00cde17bc1b7f5a4cc3ef296dcd888ca149d
Lourenço Prudêncio <lourencoprudencio@icloud.com>
2024-12-03 14:44:32 +0000

    Add files via upload

--- shop.js
+++ shop.js
@@ -18,3 +18,13 @@ searchIcon.addEventListener('click', () => {
   menu.classList.add('active');
   searchInput.focus();
 });
+// Detecta o scroll na página e ajusta o título
+document.addEventListener("scroll", () => {
+  const title = document.querySelector("main h1");
+  const scrolled = window.scrollY > 50; // Detecta se houve scroll maior que 50px
+  if (scrolled) {
+    title.classList.add("scrolled");
+  } else {
+    title.classList.remove("scrolled");
+  }
+});
